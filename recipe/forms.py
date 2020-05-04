67b15b0d555258a2375fb950b67b037670c7230f
korenenyles <korenenyles@gmail.com>
2020-05-04 11:58:07 -0400

    adds form info into forms.py

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,2 +1,30 @@
 from django import forms
+from recipe.models import Author
+"""
+class Author(models.Model):
+    name = models.CharField( max_length=50)
+    bio = models.TextField()
 
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+    time_required = models.CharField(max_length=50)
+    instructions = models.TextField()
+"""
+    
+
+class AddAuthorForm(forms.Form):
+    name = forms.CharField(max_length=50)
+    bio = forms.CharField(widget= forms.Textarea)
+
+class AddRecipeForm(forms.Form):
+    title = forms.CharField(max_length=30)
+    author = forms.ModelChoiceField(queryset=Author.objects.all())
+    time_required = forms.CharField(max_length=50)
+    description = forms.CharField(widget = forms.Textarea)
+    instructions = forms.Charfield(widget = forms.Textarea)
